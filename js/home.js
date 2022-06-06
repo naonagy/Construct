ef14a448ba7ffa99e9d34347a2a1d92e4e387785
naonagy <105072053+naonagy@users.noreply.github.com>
2022-06-06 20:22:20 +0300

    06.06

---
+++ js/home.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.slider').bxSlider(
+      {
+        auto: true,
+        stopAutoOnClick: true,
+      }
+    );
+    
+  });
---
+++ js/portofoliu.js
@@ -0,0 +1,12 @@
+import Lightbox from 'bs5-lightbox';
+
+const options = {
+	keyboard: false,
+	size: 'fullscreen'
+};
+
+document.querySelectorAll('.my-lightbox-toggle').forEach((el) => el.addEventListener('click', (e) => {
+	e.preventDefault();
+	const lightbox = new Lightbox(el, options);
+	lightbox.show();
+}));
